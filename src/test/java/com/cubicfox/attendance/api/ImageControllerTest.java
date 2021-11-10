dba07a9c12857d2805434d4650dce973d6b7015f
vptpeti <varihazy.peter@gmail.com>
2021-11-10 08:18:18 +0100

    clean up

--- src/test/java/com/cubicfox/attendance/api/ImageControllerTest.java
+++ src/test/java/com/cubicfox/attendance/api/ImageControllerTest.java
@@ -5,9 +5,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.request;
 
 import com.cubicfox.attendance.imagemaker.AttendanceProfile;
-import com.cubicfox.attendance.imagemaker.PlaceHolder;
-import java.net.URI;
-import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -18,7 +15,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @WebMvcTest(controllers = ImageController.class)
 @ComponentScan(basePackageClasses = AttendanceProfile.class)
